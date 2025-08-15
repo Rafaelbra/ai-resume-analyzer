04576c65431404022749e8e96df3b41d72a27a53
Rafael Braga <rafaelbra149@gmail.com>
2025-08-15 06:33:25 +0100

    Adding a Resume card component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,14 @@
 import type { Route } from "./+types/home";
 import NavBar from "~/components/NavBar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
   ];
-}
+}``
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -18,5 +20,14 @@ export default function Home() {
             <h2>Review your submissions and check AI-powered feedback</h2>
         </div>
     </section>
+
+      {resumes.length > 0 && (
+          <div className='resumes-section'>
+              {resumes.map((resume) => (
+                  <ResumeCard key={resume.id} resume={resume} />
+              ))}
+
+          </div>
+      )}
   </main>
 }
