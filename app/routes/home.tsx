b41aaef49faf3de88e04ca8d2d1f3bb20124f877
Rafael Braga <rafaelbra149@gmail.com>
2025-08-15 04:59:26 +0100

    Adding a NavBar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import NavBar from "~/components/NavBar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,6 +10,8 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+      <NavBar />
     <section className="main-section">
         <div className="page-heading">
             <h1>Track Your Applications & Resume ratings</h1>
