989381d98220f561f7b993a15d28c44326255e47
Rafael Braga <rafaelbra149@gmail.com>
2025-08-21 18:22:23 +0100

    Adding loading states to resume cards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import type { Route } from "./+types/home";
 import NavBar from "~/components/NavBar";
 import ResumeCard from "~/components/ResumeCard";
 import {usePuterStore} from "~/lib/puter";
-import {useNavigate} from "react-router";
+import {Link, useNavigate} from "react-router";
 import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
@@ -49,8 +49,17 @@ export default function Home() {
     <section className="main-section">
         <div className="page-heading py-16">
             <h1>Track Your Applications & Resume ratings</h1>
-            <h2>Review your submissions and check AI-powered feedback</h2>
+            {!loadingResumes && resumes.length === 0 ? (
+                <h2>No resumes found. Upload your first resume to get feedback.</h2>
+            ) : (
+                <h2>Review your submissions and check AI-powered feedback.</h2>
+            )}
         </div>
+        {loadingResumes && (
+            <div className="flex flex-col items-center justify-center">
+                <img src="/image/resume-scan-2.gif" className="w-[200px]" />
+            </div>
+        )}
 
         {!loadingResumes && resumes.length > 0 && (
             <div className='resumes-section'>
@@ -60,8 +69,13 @@ export default function Home() {
 
             </div>
         )}
+        {!loadingResumes && resumes.length === 0 && (
+            <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                <Link to='/upload' className="primary-button w-fit text-xl font-semibold">
+                    Upload Resume
+                </Link>
+            </div>
+        )}
     </section>
-
-
   </main>
 }
