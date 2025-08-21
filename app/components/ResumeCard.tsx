1bfc0e72d938fde21f4cfa6bcc1fb3a64d3031b8
Rafael Braga <rafaelbra149@gmail.com>
2025-08-21 17:39:05 +0100

    Adding structural conditioning to Resume cards

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -24,19 +24,20 @@ function ResumeCard({ resume : {id, companyName, jobTitle, feedback, imagePath} 
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-100">
             <div className='resume-card-header'>
                 <div className="flex flex-col gap-2">
-                    <h2 className="text-black font-bold - break-words">
+                    {companyName && <h2 className="text-black font-bold - break-words">
                         {companyName}
-                    </h2>
-                    <h3 className="text-lg break-words text-gray-500">
+                    </h2>}
+                    {jobTitle && <h3 className="text-lg break-words text-gray-500">
                         {jobTitle}
-                    </h3>
+                    </h3>}
+                    {!companyName && !jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
                 </div>
                 <div className='flex-shrink-0'>
                     <ScoreCircle score={feedback.overallScore} />
                 </div>
             </div>
 
-            <div className='gradient-border animate-in fade-in duration-100'>
+            {resumeUrl && (<div className='gradient-border animate-in fade-in duration-100'>
                 <div className='w-full h-full'>
                     <img
                         src={imagePath}
@@ -45,7 +46,7 @@ function ResumeCard({ resume : {id, companyName, jobTitle, feedback, imagePath} 
                     />
                 </div>
             </div>
-
+                )}
         </Link>
     )
 }
